65643716d9272a4f8b8ff3ecd45f249401b9108c
Snurre1 <thomashenriksen832@hotmail.com>
2022-05-09 13:03:07 +0200

    vcgh

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
-const url = "https://thomashenriksen.no/foodcorner/wp-json/wp/v2/posts";
-
+const url =
+  "https://thomashenriksen.no/foodcorner/wp-json/wp/v2/posts?acf_format=standard&per_page=100";
+const container = document.querySelector(".flex-container");
 async function getData() {
   try {
     const response = await fetch(url);
     const result = await response.json();
     console.log(result);
+    container.innerHTML = "";
+    for (let i = 0; i < result.length; i++) {
+      container.innerHTML += `<div><a href?"details.html?id=${result[i].id}
+                                        <h2>${result[i].title.rendered}</h2>
+                                        <img src="${result[i].featured_media}/>
+                                        </div>`;
+    }
   } catch (error) {}
 }
 getData();
